56b837c3bbe8da57c38ac9e0a7f41f5ff46d3960
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2023-06-20 21:17:38 +0200

    Project skeleton

---
+++ XPCTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XPCTest
+//
+//  Created by Ivan Sapozhnik on 20.06.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
